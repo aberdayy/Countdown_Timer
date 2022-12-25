2f82e51b2959dc0b8508da26da7386d297067075
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2022-12-25 18:27:55 +0300

    Add files via upload

---
+++ CountDownTimerApp.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print('Timer Completed!')
+    
+seconds = input('Enter the time in seconds : ')
+countdown(int(seconds))
